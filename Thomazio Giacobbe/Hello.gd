caa4321276cfe8eca5e807abe5d7fb4012a631a9
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-11-24 21:36:52 -0300

    hello world

---
+++ Thomazio Giacobbe/Hello.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var nome = "Thomazio"
+	print("Hello " + nome + "!") # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
